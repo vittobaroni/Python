93a6b7e7149eccfb72e76faa00afa80c4802e270
Vitto Baroni <vitto.baroni18@gmail.com>
2023-07-04 13:07:31 -0300

    atualizando ex006

--- Exercícios/ex006.py
+++ Exercícios/ex006.py
@@ -2,4 +2,4 @@
 
 numero = int(input('Digite um número inteiro: '))
 
-print('Você digitou o número {}\nSeu dobro é {}\nSeu triplo é {}\nE sua raíz quadrada é {:.0f}'.format(numero,numero*2, numero*3, numero**(1/2)))
+print('Você digitou o número {}\nSeu dobro é {}\nSeu triplo é {}\nE sua raíz quadrada é {:.2f}'.format(numero, numero*2, numero*3, numero**(1/2)))
